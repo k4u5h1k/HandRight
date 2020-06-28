872518e726a081d05df00d840d55dd660d8d4494
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2020-06-29 03:39:16 +0530

    Welcome to HandRight

---
+++ readmefirst.txt
@@ -0,0 +1,8 @@
+- THIS PROJECT ONLY SUPPORTS PYTHON3.6
+1. Start a virtual environment following steps suitable to your os
+2. run python3.6 -m pip install -r requirements.txt
+3. start django server using python3.6 src/manage.py runserver
+4. navigate to the prompted url to access application
+5. PROFIT
+
+If you run into a locale error set the approprate environment variable after googling that error
---
+++ src/Mainapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.files.storage import default_storage
+from PyPDF2 import PdfFileReader
+from .handright.main import *
+import os
+import time
+from atlas import settings
+
+def home(request):
+    return render(request,"index.html",{})
+
+def forminput(request):
+    if request.method=='POST':
+        dic = dict(request.POST)
+        fl = request.FILES['pdf']
+        filename = settings.BASE_DIR +'/Mainapp/handright/pdf/upload.pdf'
+        with open(filename,'wb+') as handle:
+            for chunk in fl.chunks():
+                handle.write(chunk)
+        # print(settings.pdf_file['pdf'].read())
+        request.session['hello'] =[int(dic['style'][0]), dic['color'][0]] 
+        return render(request, "loading.html", {'load' : True})
+    return render(request,"index.html",{})
+
+def ML(request):
+    data = request.session.get('hello')
+    style = data[0]
+    color = data[1]
+    # fl = settings.pdf_file['pdf']
+    # print(fl.read())
+    lets_go(style, color)
+    return render(request, "loaded.html", {})
